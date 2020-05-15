8afd134a817508e071e52b2ec6cdb8d34341a88a
dianmjs <dianamjs69@gmail.com>
2020-05-15 16:12:11 -0500

    filtros

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import posts from './posts'
-
+import React, { useState, useEffect } from "react";
+import posts from "./posts";
 
 // Modifica el componente App para implmentar la funcionalidad requerida
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
-        </div>
-        <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
-        </ul>
+function App() {
+  const [searchList, setSearchlist] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleChange = (event) => {
+    setSearchlist(event.target.value);
+  };
+
+  useEffect(() => {
+    const results = posts.title.filter((postes) =>
+      postes.toLowerCase().includes(searchList)
+    );
+    setSearchResults(results);
+  }, [searchList]);
+
+  return (
+    <div>
+      <div className="filter">
+        <input
+          type="text"
+          placeholder="Ingresa el término de búsqueda"
+          value={searchList}
+          onChange={handleChange}
+        />
       </div>
-    )
-  }
+      <ul>
+        <li>
+          {searchResults.map((item) => (
+            <li>{item}</li>
+          ))}
+          <a href={posts[0].url}>
+            <img src={posts[0].image} />
+          </a>
+          <p>{posts[0].title}</p>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-
-export default App
-
-
+export default App;
